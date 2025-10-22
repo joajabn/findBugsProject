f3767c405e2b8333c76c911d13d431623d4baedd
joajabn <jablonskaa.joanna@gmail.com>
2025-10-22 19:06:47 +0200

    refactor guthub url

--- src/main/java/pl/paniodprogramowania/findBugsProject/github/retrofit/RetrofitConfiguration.java
+++ src/main/java/pl/paniodprogramowania/findBugsProject/github/retrofit/RetrofitConfiguration.java
@@ -1,5 +1,6 @@
 package pl.paniodprogramowania.findBugsProject.github.retrofit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -8,13 +9,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @Configuration
 public class RetrofitConfiguration {
 
-  @Bean
-  public GithubApi githubApi(){
-    Retrofit retrofit = new Retrofit.Builder()
-        .baseUrl("https://api.github.com/")
-        .addConverterFactory(GsonConverterFactory.create()) //zamienia json na klasy
-        .build();
+    @Value("${api.github.url}")
+    private String githubUrl;
 
-    return retrofit.create(GithubApi.class);
-  }
+    @Bean
+    public GithubApi githubApi() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(githubUrl)
+                .addConverterFactory(GsonConverterFactory.create()) //zamienia json na klasy
+                .build();
+
+        return retrofit.create(GithubApi.class);
+    }
 }
